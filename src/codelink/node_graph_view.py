194d1726edab2749c1cce1c597fafc8a8fbced54
j8sr0230 <ronny.scharf08@gmail.com>
2023-01-29 21:47:14 +0100

    Introduces draw_grid method in node_graph_view.py

--- src/codelink/node_graph_view.py
+++ src/codelink/node_graph_view.py
@@ -1,6 +1,16 @@
 import tkinter as tk
 
 
+SCENE_SIZE = 4000
+SCENE_COLOR = "#1D1D1D"
+
+MAJOR_DOT_SIZE = 4
+MAJOR_STEP = 100
+MINOR_DOT_SIZE = 2
+MINOR_STEP = 20
+GRID_COLOR = "white"  # "#282828"
+
+
 class NodeGraphView(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
@@ -11,7 +21,7 @@ class NodeGraphView(tk.Frame):
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=0)
 
-        self.node_graph_scene = tk.Canvas(self, bg="#FFFFFF", scrollregion=(0, 0, 10000, 10000))
+        self.node_graph_scene = tk.Canvas(self, bg=SCENE_COLOR, scrollregion=(0, 0, SCENE_SIZE, SCENE_SIZE))
         self.node_graph_scene.grid(row=0, column=0, sticky=tk.N+tk.S+tk.W+tk.E)
 
         self.hor_bar = tk.Scrollbar(self, orient=tk.HORIZONTAL)
@@ -22,10 +32,25 @@ class NodeGraphView(tk.Frame):
         self.ver_bar.config(command=self.node_graph_scene.yview)
         self.node_graph_scene.config(xscrollcommand=self.hor_bar.set, yscrollcommand=self.ver_bar.set)
 
-        self.controller = None
+        self.draw_grid()
 
-        # Test drawing
-        self.node_graph_scene.create_line(10, 10, 1000, 500)
+        self.controller = None
 
     def set_controller(self, controller):
         self.controller = controller
+
+    def draw_grid(self):
+        self.update()
+        # left_bound = int(self.node_graph_scene.xview()[0] * SCENE_SIZE)
+        right_bound = int(self.node_graph_scene.xview()[1] * SCENE_SIZE)
+        # top_bound = int(self.node_graph_scene.yview()[0] * SCENE_SIZE)
+        bottom_bound = int(self.node_graph_scene.yview()[1] * SCENE_SIZE)
+
+        for x in range(right_bound):
+            for y in range(bottom_bound):
+                if (x % MAJOR_STEP == 0) and (y % MAJOR_STEP == 0):
+                    self.node_graph_scene.create_oval(x, y, x + MAJOR_DOT_SIZE, y + MAJOR_DOT_SIZE, width=0,
+                                                      fill=GRID_COLOR)
+                elif (x % MINOR_STEP == 0) and (y % MINOR_STEP == 0):
+                    self.node_graph_scene.create_oval(x, y, x + MINOR_DOT_SIZE, y + MINOR_DOT_SIZE, width=0,
+                                                      fill=GRID_COLOR)
