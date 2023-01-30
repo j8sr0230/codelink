6da40a15ae47eb53f82cfbe573e2f296934424bf
j8sr0230 <ronny.scharf08@gmail.com>
2023-01-30 09:38:24 +0100

    Removes minor grid

--- src/codelink/node_graph_view.py
+++ src/codelink/node_graph_view.py
@@ -1,14 +1,12 @@
 import tkinter as tk
 
 
-SCENE_SIZE = 4000
+SCENE_SIZE = 10000
 SCENE_COLOR = "#1D1D1D"
 
-MAJOR_DOT_SIZE = 4
-MAJOR_STEP = 100
-MINOR_DOT_SIZE = 2
-MINOR_DIV = 4
-GRID_COLOR = "#282828"
+GRID_DOT_SIZE = 4
+GRID_STEP = 100
+GRID_COLOR = "#393939"
 
 
 class NodeGraphView(tk.Frame):
@@ -33,40 +31,28 @@ class NodeGraphView(tk.Frame):
 
         self.pack(fill="both", expand=True)
 
-        self.grid = []
         self.controller = None
 
     def set_controller(self, controller):
         self.controller = controller
 
     def draw_grid(self):
-        if len(self.grid) > 0:
-            for grid_dot in self.grid:
-                self.node_graph_scene.delete(grid_dot)
-            self.grid = []
+        self.node_graph_scene.delete("grid")
 
-        left_bound = int(self.node_graph_scene.xview()[0] * SCENE_SIZE)
-        right_bound = int(self.node_graph_scene.xview()[1] * SCENE_SIZE)
-        top_bound = int(self.node_graph_scene.yview()[0] * SCENE_SIZE)
-        bottom_bound = int(self.node_graph_scene.yview()[1] * SCENE_SIZE)
+        viewport_left = int(self.node_graph_scene.xview()[0] * SCENE_SIZE)
+        viewport_right = int(self.node_graph_scene.xview()[1] * SCENE_SIZE)
+        viewport_top = int(self.node_graph_scene.yview()[0] * SCENE_SIZE)
+        viewport_bottom = int(self.node_graph_scene.yview()[1] * SCENE_SIZE)
 
-        left_pixel = MAJOR_STEP * (left_bound//MAJOR_STEP)
-        right_pixel = MAJOR_STEP * (right_bound//MAJOR_STEP) + MAJOR_STEP
-        top_pixel = MAJOR_STEP * (top_bound//MAJOR_STEP)
-        bottom_pixel = MAJOR_STEP * (bottom_bound//MAJOR_STEP) + MAJOR_STEP
+        grid_left = GRID_STEP * (viewport_left // GRID_STEP)
+        grid_right = GRID_STEP * (viewport_right // GRID_STEP) + GRID_STEP
+        grid_top = GRID_STEP * (viewport_top // GRID_STEP)
+        grid_bottom = GRID_STEP * (viewport_bottom // GRID_STEP) + GRID_STEP
 
-        for x in range(left_pixel, right_pixel, MAJOR_STEP):
-            for y in range(top_pixel, bottom_pixel, MAJOR_STEP):
-                grid_dot = self.node_graph_scene.create_oval(x, y, x + MAJOR_DOT_SIZE, y + MAJOR_DOT_SIZE,
-                                                             width=0, fill=GRID_COLOR)
-                self.grid.append(grid_dot)
-        for x in range(left_pixel, right_pixel, MAJOR_STEP//MINOR_DIV):
-            for y in range(top_pixel, bottom_pixel, MAJOR_STEP//MINOR_DIV):
-                grid_dot = self.node_graph_scene.create_oval(x, y, x + MINOR_DOT_SIZE, y + MINOR_DOT_SIZE,
-                                                             width=0, fill=GRID_COLOR)
-                self.grid.append(grid_dot)
-
-        # print(len(self.grid))
+        for x in range(grid_left, grid_right, GRID_STEP):
+            for y in range(grid_top, grid_bottom, GRID_STEP):
+                self.node_graph_scene.create_oval(x, y, x + GRID_DOT_SIZE, y + GRID_DOT_SIZE, width=0,
+                                                  fill=GRID_COLOR, tags="grid")
 
     def on_x_scroll(self, first, last):
         self.hor_bar.set(first, last)
