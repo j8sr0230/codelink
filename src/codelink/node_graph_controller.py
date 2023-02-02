c9945265bb448cd372015d9633b34c2242099cbd
j8sr0230 <ronny.scharf08@gmail.com>
2023-02-02 08:35:38 +0100

    Adds mouse control to node graph

--- src/codelink/node_graph_controller.py
+++ src/codelink/node_graph_controller.py
@@ -3,32 +3,51 @@ class NodeGraphController:
         self.mode = model
         self.view = view
 
+        self.left_mouse_down = False
         self.selected_item = None
         self.selected_item_origin = None
-        self.selected_position = None
+        self.event_start_position = None
 
-    def register_selection(self, event):
-        if not self.selected_item:
-            items = self.view.find_withtag("current")
-            if len(items) > 0:
-                self.selected_item = items[0]
-                self.selected_item_origin = self.view.coords(items[0])[:2]
-                self.selected_position = (self.view.canvasx(event.x), self.view.canvasy(event.y))
+    def move_from(self, event):
+        self.left_mouse_down = True
+
+        items = self.view.find_withtag("current")
+        if len(items) > 0:
+            # If item selected, save item, origin and event position
+            self.selected_item = items[0]
+            self.selected_item_origin = self.view.coords(items[0])[:2]
+            self.event_start_position = (self.view.canvasx(event.x), self.view.canvasy(event.y))
+
+            # Modify selected canvas item
+            self.view.tag_raise(items[0])
+            self.view.itemconfig(items[0], width=3)
+        else:
+            # If nothing selected, prepare for canvas dragging
+            self.view.scan_mark(event.x, event.y)
+
+    def move(self, event):
+        if self.left_mouse_down:
+            if self.selected_item:
+                # If item selected, move it to current mouse position
+                current_mouse_position = (self.view.canvasx(event.x), self.view.canvasy(event.y))
+                item_origin_offset_x = self.event_start_position[0] - self.selected_item_origin[0]
+                item_origin_offset_y = self.event_start_position[1] - self.selected_item_origin[1]
+                self.view.moveto(self.selected_item, current_mouse_position[0] - item_origin_offset_x,
+                                 current_mouse_position[1] - item_origin_offset_y)
             else:
-                self.selected_item = None
-                self.selected_item_origin = None
-                self.selected_position = None
+                # If nothing selected, move canvas to current mouse position
+                self.view.scan_dragto(event.x, event.y, gain=1)
+        self.view.update()
 
-    def clear_selection(self, event):
-        self.selected_item = None
-        self.selected_item_origin = None
-        self.selected_position = None
+    # noinspection PyUnusedLocal
+    def move_to(self, event):
+        self.left_mouse_down = False
 
-    def move_selection(self, event):
         if self.selected_item:
-            current_position = (self.view.canvasx(event.x), self.view.canvasy(event.y))
-            offset_x = self.selected_position[0] - self.selected_item_origin[0]
-            offset_y = self.selected_position[1] - self.selected_item_origin[1]
+            # Reset selected canvas item
+            self.view.itemconfig(self.selected_item, width=1)
 
-            self.view.moveto(self.selected_item, current_position[0] - offset_x,
-                             current_position[1] - offset_y)
+            # Clear selection
+            self.selected_item = None
+            self.selected_item_origin = None
+            self.event_start_position = None
--- src/codelink/node_graph_view.py
+++ src/codelink/node_graph_view.py
@@ -9,23 +9,24 @@ class NodeGraphView(tk.Canvas):
         super().__init__(parent, bg=SCENE_COLOR)
         self.pack(fill="both", expand=True)
 
-        self.bind("<Button-1>", self.on_left_down)
-        self.bind("<ButtonRelease-1>", self.on_left_up)
-        self.bind("<Motion>", self.on_move)
+        self.bind("<Button-1>", self.on_mouse_left_down)
+        self.bind("<ButtonRelease-1>", self.on_mouse_left_up)
+        self.bind("<Motion>", self.on_mouse_move)
 
         self.controller = None
 
-        # Draw test object
-        self.create_rectangle([10, 10, 160, 110], fill="#292929", outline="#898989", width=1)
+        # Draw test items
+        self.create_rectangle([10, 10, 160, 110], fill="#292929", outline="#898989", width=1, tags="node")
+        self.create_rectangle([100, 100, 260, 210], fill="#292929", outline="#898989", width=1, tags="node")
 
     def set_controller(self, controller):
         self.controller = controller
 
-    def on_left_down(self, event):
-        self.controller.register_selection(event)
+    def on_mouse_left_down(self, mouse_event):
+        self.controller.move_from(mouse_event)
 
-    def on_left_up(self, event):
-        self.controller.clear_selection(event)
+    def on_mouse_left_up(self, mouse_event):
+        self.controller.move_to(mouse_event)
 
-    def on_move(self, event):
-        self.controller.move_selection(event)
+    def on_mouse_move(self, mouse_event):
+        self.controller.move(mouse_event)
