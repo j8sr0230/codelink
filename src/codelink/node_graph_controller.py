00384faa481f879c47c5c69de5c37cf647a38a45
j8sr0230 <ronny.scharf08@gmail.com>
2023-02-08 15:54:18 +0100

    Adds mouse state to node_graph_controller.py

--- src/codelink/node_graph_controller.py
+++ src/codelink/node_graph_controller.py
@@ -1,4 +1,4 @@
-from node_graph_view import RESIZE_SQUARE
+from node_graph_view import RESIZE_BORDER_WIDTH
 
 
 class NodeGraphController:
@@ -6,71 +6,69 @@ class NodeGraphController:
         self.mode = model
         self.view = view
 
-        self.left_mouse_down = False
+        self.mouse_mode = None
         self.selected_item = None
-        self.item_event_offset = None
+        self.item_click_offset = None
 
     def move_from(self, mouse_event):
-        self.left_mouse_down = True
-
         items = self.view.find_withtag("current")
-        if len(items) > 0 and "node" in self.view.gettags(items[0]):
-            # If selected item is node, save item and offset between event and item origin
-            self.selected_item = items[0]
-            self.item_event_offset = (self.view.canvasx(mouse_event.x) - self.view.coords(items[0])[0],
-                                      self.view.canvasy(mouse_event.y) - self.view.coords(items[0])[1])
 
-            # Modify selected item
-            self.view.tag_raise(items[0])
-            self.view.itemconfig(items[0], width=1)
+        if len(items) > 0:
+            if "node" in self.view.gettags(items[0]):
+                # If selected item is node, save item and offset between click event and item origin
+                self.selected_item = items[0]
+                self.item_click_offset = (self.view.canvasx(mouse_event.x) - self.view.coords(items[0])[0],
+                                          self.view.canvasy(mouse_event.y) - self.view.coords(items[0])[1])
+
+                # Modify selected item
+                self.view.tag_raise(items[0])
+                self.view.itemconfig(items[0], width=3)
         else:
             # If no node selected, prepare for canvas dragging
             self.view.scan_mark(mouse_event.x, mouse_event.y)
 
     def move(self, mouse_event):
-        if self.left_mouse_down:
-            if self.selected_item:
-                # If node selected
-                current_mouse_position = (self.view.canvasx(mouse_event.x), self.view.canvasy(mouse_event.y))
+        if self.selected_item:
+            current_mouse_position = (self.view.canvasx(mouse_event.x), self.view.canvasy(mouse_event.y))
 
-                # Calculate current node and event data
-                item_coords = self.view.coords(self.selected_item)
-                current_item_width = item_coords[2] - item_coords[0]
-                current_item_height = item_coords[3] - item_coords[1]
-                resize_x_area_start = item_coords[0] + current_item_width - RESIZE_SQUARE
-                resize_y_area_start = item_coords[1] + current_item_height - RESIZE_SQUARE
+            # Calculate current item and event data
+            item_coords = self.view.coords(self.selected_item)
+            current_item_width = item_coords[2] - item_coords[0]
+            resize_x_area_start = item_coords[0] + current_item_width - self.view.get_scale() * RESIZE_BORDER_WIDTH
 
-                if (current_mouse_position[0] > resize_x_area_start) and \
-                        (current_mouse_position[1] > resize_y_area_start):
-                    # If mouse position within resize area, resize node
-                    self.view.coords(self.selected_item, item_coords[0], item_coords[1], current_mouse_position[0],
-                                     item_coords[3])
-                else:
-                    # If mouse position outside resize area, move node
-                    origin = self.view.get_grid_origin()
-                    minor = self.view.get_minor_width()
-
-                    self.view.moveto(self.selected_item,
-                                     round((((current_mouse_position[0] - self.item_event_offset[0] - origin[0]) //
-                                             minor) * minor) + origin[0]),
-                                     round((((current_mouse_position[1] - self.item_event_offset[1] - origin[1]) //
-                                             minor) * minor) + origin[1]))
-
+            # Set mouse mode
+            if current_mouse_position[0] > resize_x_area_start:
+                if not self.mouse_mode:
+                    self.mouse_mode = "resize_item"
             else:
-                # If nothing selected, move canvas to current mouse position
+                if not self.mouse_mode:
+                    self.mouse_mode = "move_item"
+
+            # Do action
+            if self.mouse_mode == "resize_item":
+                self.view.config(cursor="size_we")
+                self.view.coords(self.selected_item, item_coords[0], item_coords[1], current_mouse_position[0],
+                                 item_coords[3])
+            elif self.mouse_mode == "move_item":
+                snapped_xy = self.view.snap_to_grid(current_mouse_position, self.item_click_offset)
+                self.view.moveto(self.selected_item, snapped_xy[0], snapped_xy[1])
+        else:
+            # If nothing selected, move canvas to current mouse position
+            self.mouse_mode = "scroll_canvas"
+            if self.mouse_mode == "scroll_canvas":
                 self.view.scan_dragto(mouse_event.x, mouse_event.y, gain=1)
 
     # noinspection PyUnusedLocal
     def move_to(self, mouse_event):
-        self.left_mouse_down = False
-
         if self.selected_item:
             # Reset selected item
             self.view.itemconfig(self.selected_item, width=1)
 
             # Clear selection
+            self.mouse_mode = None
             self.selected_item = None
-            self.item_event_offset = None
+            self.item_click_offset = None
+            self.view.config(cursor="arrow")
 
     def zoom(self, mouse_event):
         if mouse_event.delta > 0:
@@ -80,7 +78,7 @@ class NodeGraphController:
             self.view.scale("all", self.view.canvasx(mouse_event.x), self.view.canvasy(mouse_event.y), 0.9, 0.9)
             self.view.update_scale(0.9)
 
-        if self.view.scene_scale < 0.8:
+        if self.view.scene_scale < 0.7:
             self.view.itemconfigure("grid", state="hidden")
         else:
             self.view.itemconfigure("grid", state="normal")
--- src/codelink/node_graph_view.py
+++ src/codelink/node_graph_view.py
@@ -2,15 +2,15 @@ import tkinter as tk
 
 
 DARK_BACKGROUND = "#1D1D1D"
-LIGHT_BACKGROUND = "#606060"
+LIGHT_BACKGROUND = "#383838"
 FOREGROUND = "#292929"
 
-GRID_WIDTH = 2000
+GRID_WIDTH = 5000
 MINOR_TICK = 20
 MAJOR_TICK_FACTOR = 5
 MARKER_RADIUS = 4
 
-RESIZE_SQUARE = 20
+RESIZE_BORDER_WIDTH = 50
 DEFAULT_FONT = "Helvetica 12"
 
 
@@ -21,11 +21,11 @@ class NodeGraphView(tk.Canvas):
 
         self.info_label = tk.Label(self, text="1.0", font=DEFAULT_FONT, bg=DARK_BACKGROUND, fg=LIGHT_BACKGROUND)
         self.info_label.pack(side=tk.BOTTOM, anchor=tk.SW, padx=10, pady=10)
-
-        self.bind("<Button-1>", self.on_mouse_left_down)
+        self.bind("<ButtonPress-1>", self.on_mouse_left_down)
         self.bind("<ButtonRelease-1>", self.on_mouse_left_up)
-        self.bind("<Motion>", self.on_mouse_move)
+        self.bind("<B1-Motion>", self.on_mouse_move)
         self.bind("<MouseWheel>", self.on_mouse_wheel)
+
         self.controller = None
         self.scene_scale = 1.0
 
@@ -34,15 +34,16 @@ class NodeGraphView(tk.Canvas):
         self.itemconfigure("minor_tick", state="hidden")
 
         # Draw background
-        for x in range(-GRID_WIDTH, GRID_WIDTH + 1, MINOR_TICK*MAJOR_TICK_FACTOR):
-            for y in range(-GRID_WIDTH, GRID_WIDTH + 1, MINOR_TICK*MAJOR_TICK_FACTOR):
+        for x in range(-GRID_WIDTH, GRID_WIDTH, MINOR_TICK * MAJOR_TICK_FACTOR):
+            for y in range(-GRID_WIDTH, GRID_WIDTH + 1, MINOR_TICK * MAJOR_TICK_FACTOR):
                 self.create_oval((x - MARKER_RADIUS, y - MARKER_RADIUS, x + MARKER_RADIUS, y + MARKER_RADIUS),
                                  width=1, outline=LIGHT_BACKGROUND, fill=LIGHT_BACKGROUND, tags="grid")
 
         # Draw test nodes
         self.create_rectangle([10, 10, 160, 110], fill=FOREGROUND, outline="red", width=1, tags="node")
-        self.create_rectangle([100, 100, 260, 210], fill=FOREGROUND, outline=LIGHT_BACKGROUND, width=1, tags="node")
-        # self.tag_bind('node', '<Enter>', print)
+        self.create_rectangle([100, 100, 260, 210], fill=FOREGROUND, outline="green", width=1, tags="node")
+        self.tag_bind('node', '<Enter>', self.on_enter_item)
+        self.tag_bind('node', '<Leave>', self.on_leave_item)
 
     def on_mouse_left_down(self, mouse_event):
         if self.controller:
@@ -60,14 +61,32 @@ class NodeGraphView(tk.Canvas):
         if self.controller:
             self.controller.zoom(mouse_event)
 
+    def on_enter_item(self, enter_event):
+        pass
+        # current_mouse_position = (self.canvasx(enter_event.x), self.canvasy(enter_event.y))
+        # selected_item = self.find_closest(current_mouse_position[0], current_mouse_position[1])[0]
+        # item_coords = self.coords(selected_item)
+        # item_width = item_coords[2] - item_coords[0]
+        # resize_x_area_start = item_coords[0] + item_width - self.get_scale() * RESIZE_BORDER_WIDTH
+        # if current_mouse_position[0] > resize_x_area_start:
+        #     self.config(cursor="size_we")
+
+    # noinspection PyUnusedLocal
+    def on_leave_item(self, enter_event):
+        pass
+        # self.config(cursor="arrow")
+
     def set_controller(self, controller):
         self.controller = controller
 
-    def update_scale(self, scale):
-        self.scene_scale *= scale
-        self.set_info_text("{0:.1f}".format(self.scene_scale))
+    def get_scale(self):
+        return self.scene_scale
 
-    def set_info_text(self, msg):
+    def update_scale(self, multiplier):
+        self.scene_scale *= multiplier
+        self.update_info_text("{0:.1f}".format(self.scene_scale))
+
+    def update_info_text(self, msg):
         self.info_label.config(text=msg)
 
     def get_minor_width(self):
@@ -79,3 +98,11 @@ class NodeGraphView(tk.Canvas):
         item_coords = self.coords(grid_origin_item)
         item_center = ((item_coords[0] + item_coords[2]) / 2, (item_coords[1] + item_coords[3]) / 2)
         return item_center
+
+    def snap_to_grid(self, position, item_click_offset):
+        origin = self.get_grid_origin()
+        minor = self.get_minor_width()
+
+        x_snap = round((((position[0] - item_click_offset[0] - origin[0]) // minor) * minor) + origin[0])
+        y_snap = round((((position[1] - item_click_offset[1] - origin[1]) // minor) * minor) + origin[1])
+        return x_snap, y_snap
