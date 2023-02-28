378e9baf7610d80e638f06a13dd0d3e5d7a182d6
j8sr0230 <ronny.scharf08@gmail.com>
2023-02-28 09:50:10 +0100

    Introduces node graph model as QStandardItemModel

--- src/codelink/app.py
+++ src/codelink/app.py
@@ -3,12 +3,29 @@ from PySide2.QtCore import Qt, QModelIndex
 
 
 if __name__ == "__main__":
-    model: QStandardItemModel = QStandardItemModel()
-    parent_item: QStandardItem = model.invisibleRootItem()
-    item: QStandardItem = QStandardItem("Hello World")
-    parent_item.appendRow(item)
+    # Creates node graph model as QStandardItemModel and registers listeners
+    node_graph: QStandardItemModel = QStandardItemModel()
+    parent_node: QStandardItem = node_graph.invisibleRootItem()
+    node_graph.rowsInserted.connect(lambda insert_event: print("Row inserted", insert_event))
 
-    for row in range(model.rowCount()):
-        idx: QModelIndex = model.index(row, 0, QModelIndex())
-        data: str = model.data(idx, 0)
-        print(data)
+    # Creates and inserts nodes as QStandardItems into the node graph
+    node_a: QStandardItem = QStandardItem("Node A")
+    node_a.setData({
+        "Title": node_a.text(),
+        "inputs": [1, 2, 3],
+        "outputs": [4, 5, 6]
+        })
+    parent_node.appendRow(node_a)
+
+    node_b: QStandardItem = QStandardItem("Node B")
+    node_b.setData({
+        "Title": node_b.text(),
+        "inputs": [1, 2, 3],
+        "outputs": [4, 5, 6]
+    })
+    parent_node.appendRow(node_b)
+
+    # Loops over node graph rows and prints selected data
+    for row in range(node_graph.rowCount()):
+        node: QStandardItem = node_graph.item(row, 0)
+        print(node.data())
