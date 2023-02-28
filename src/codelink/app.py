0fbb94d1313a38c016918063ecd2ac3bc1f38c0c
j8sr0230 <ronny.scharf08@gmail.com>
2023-02-28 09:02:34 +0100

    Restarts project with PySide2

--- src/codelink/app.py
+++ src/codelink/app.py
@@ -1,7 +1,14 @@
-class App:
-    def __init__(self):
-        print("Hello World")
+from PySide2.QtGui import QStandardItemModel, QStandardItem
+from PySide2.QtCore import Qt, QModelIndex
 
 
 if __name__ == "__main__":
-    app = App()
+    model: QStandardItemModel = QStandardItemModel()
+    parent_item: QStandardItem = model.invisibleRootItem()
+    item: QStandardItem = QStandardItem("Hello World")
+    parent_item.appendRow(item)
+
+    for row in range(model.rowCount()):
+        idx: QModelIndex = model.index(row, 0, QModelIndex())
+        data: str = model.data(idx, 0)
+        print(data)
