42e25217f5194d3560ea01f793ffef6db2f2b29e
j8sr0230 <ronny.scharf08@gmail.com>
2023-03-04 20:38:40 +0100

    Adds BasicNode to main.py

--- src/codelink/main.py
+++ src/codelink/main.py
@@ -1,40 +1,61 @@
 import time
 
 from dask.threaded import get
-# from dask.delayed import Delayed
-
+from dask.delayed import Delayed
 from FreeCAD import Vector
 
 
-def add(a, b):
-    return a + b
+class BasicNode:
+    def __init__(self, input_labels: tuple = ("A", "B"), output_labels: tuple = ("C", )):
+        self.input_labels: tuple = input_labels
+        self.output_labels: tuple = output_labels
+
+        self.data: int = 0
+
+    def eval(self, a, b):
+        return a + b
+
+    def to_dict(self):
+        eval_result = dict()
+        eval_result[self.output_labels[0]] = (self.eval, 1, 2)
+        return eval_result
 
 
-# Build graph as dict
-in_a = 5
-dsk = {"x": in_a,
-       "y": 2,
-       "z": (add, "x", "y"),
-       "w": (sum, ["x", "y", "z"]),
-       "vec": (Vector, ["x", "y", "z"])
-       }
+n = BasicNode(("x", "y"), ("z", ))
+dsk = n.to_dict()
+result = get(dsk, "z")
+print(result)
 
-# Start timer for performance measurement
-start = time.time()
 
-# Wrapping dsk in a Dask Collection (Delayed)
-# delayed_dsk = Delayed("vec", dsk)
-
-# Compute and print result
-# result = delayed_dsk.compute()
-# print(result)
-result = get(dsk, "vec")
-
-# Stop timer for performance measurement and print time and result
-end = time.time()
-print("Execution result:", result)
-print("Execution time:", round(end - start, 4), "s")
-
-# Visualize graph
-# delayed_dsk.visualize()
-# dask.visualize(delayed_dsk)
+# def add(a, b):
+#     return a + b
+#
+#
+# # Build graph as dict
+# in_a = 5
+# dsk = {"x": in_a,
+#        "y": 2,
+#        "z": (add, "x", "y"),
+#        "w": (sum, ["x", "y", "z"]),
+#        "vec": (Vector, ["x", "y", "z"])
+#        }
+#
+# # Start timer for performance measurement
+# start = time.time()
+#
+# # Wrapping dsk in a Dask Collection (Delayed)
+# # delayed_dsk = Delayed("vec", dsk)
+#
+# # Compute and print result
+# # result = delayed_dsk.compute()
+# # print(result)
+# result = get(dsk, "vec")
+#
+# # Stop timer for performance measurement and print time and result
+# end = time.time()
+# print("Execution result:", result)
+# print("Execution time:", round(end - start, 4), "s")
+#
+# # Visualize graph
+# # delayed_dsk.visualize()
+# # dask.visualize(delayed_dsk)
