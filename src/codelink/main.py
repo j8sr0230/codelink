a586fd9f5c1754e750e621340e400e512700e231
j8sr0230 <ronny.scharf08@gmail.com>
2023-03-06 17:35:07 +0100

    Adds some tasks to main.py

--- src/codelink/main.py
+++ src/codelink/main.py
@@ -2,29 +2,33 @@ import time
 
 from dask.threaded import get
 from dask.delayed import Delayed
+
 from FreeCAD import Vector
 
-
-class BasicNode:
-    def __init__(self, input_labels: tuple = ("A", "B"), output_labels: tuple = ("C", )):
-        self.input_labels: tuple = input_labels
-        self.output_labels: tuple = output_labels
-
-        self.data: int = 0
-
-    def eval(self, a, b):
-        return a + b
-
-    def to_dict(self):
-        eval_result = dict()
-        eval_result[self.output_labels[0]] = (self.eval, 1, 2)
-        return eval_result
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
-n = BasicNode(("x", "y"), ("z", ))
-dsk = n.to_dict()
-result = get(dsk, "z")
-print(result)
+# class BasicNode:
+#     def __init__(self, input_labels: tuple = ("A", "B"), output_labels: tuple = ("C", )):
+#         self.input_labels: tuple = input_labels
+#         self.output_labels: tuple = output_labels
+#
+#         self.data: int = 0
+#
+#     def eval(self, a, b):
+#         return a + b
+#
+#     def to_dict(self):
+#         eval_result = dict()
+#         eval_result[self.output_labels[0]] = (self.eval, 1, 2)
+#         return eval_result
+#
+#
+# n = BasicNode(("x", "y"), ("z", ))
+# dsk = n.to_dict()
+# result = get(dsk, "z")
+# print(result)
 
 
 # def add(a, b):
@@ -59,3 +63,65 @@ print(result)
 # # Visualize graph
 # # delayed_dsk.visualize()
 # # dask.visualize(delayed_dsk)
+
+
+class InputTask:
+    def __init__(self, a: int = 0):
+        self.a = a
+
+    def eval(self) -> int:
+        return self.a
+
+
+class DefaultTask:
+    @staticmethod
+    def eval(a: int = 0, b: int = 0) -> int:
+        return a + b
+
+
+t1: InputTask = InputTask(1)
+t2: InputTask = InputTask(9)
+t3: DefaultTask = DefaultTask()
+t4: DefaultTask = DefaultTask()
+
+dsk = {
+    t1: (t1.eval, ),
+    t2: (t2.eval, ),
+    t3: (t3.eval, t1, t2),
+    t4: (t4.eval, t1, t3)
+}
+
+result = get(dsk, t4)
+print(result)
+
+G = nx.DiGraph()
+G.add_node(t1)
+G.add_node(t2)
+G.add_node(t3)
+G.add_node(t4)
+
+G.add_edge(t1, t3)
+G.add_edge(t2, t3)
+G.add_edge(t3, t4)
+
+
+def resolve_di_graph(graph: nx.DiGraph, node, level: int = 0) -> None:
+    print(node, level)
+    predecessors = graph.predecessors(node)
+    for node in predecessors:
+        sub_level: int = level + 1
+        resolve_di_graph(graph, node, sub_level)
+
+
+resolve_di_graph(G, t4)
+
+labels = dict()
+labels[t1] = r"$t_1$"
+labels[t2] = r"$t_2$"
+labels[t3] = r"$t_3$"
+labels[t4] = r"$t_4$"
+
+pos = nx.spectral_layout(G)
+nx.draw_networkx_labels(G, pos, labels)
+nx.draw(G, pos, with_labels=False)
+plt.show()
