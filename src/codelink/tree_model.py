fa38647ec7fc4677d3f9b078a4b58cc06768c005
j8sr0230 <ronny.scharf08@gmail.com>
2023-03-16 20:30:19 +0100

    Starts tree model implementation

--- src/codelink/tree_model.py
+++ src/codelink/tree_model.py
@@ -1,4 +1,7 @@
-from PySide2.QtGui import QStandardItem
+import sys
+
+from PySide2.QtGui import QStandardItem, QStandardItemModel
+from PySide2.QtWidgets import QApplication, QTreeView
 from PySide2.QtCore import Qt
 
 
@@ -12,19 +15,35 @@ class QNodeGraphItem(QStandardItem):
 		self.appendRow(self.task)
 
 		self.predecessors = QStandardItem()
-		self.predecessors.setData([1, 2, 3], Qt.DisplayRole)
+		self.predecessors.setData("[1, 2, 3]", Qt.DisplayRole)
 		self.appendRow(self.predecessors)
 
 		self.successor = QStandardItem()
-		self.successor.setData([], Qt.DisplayRole)
+		self.successor.setData("[]", Qt.DisplayRole)
 		self.appendRow(self.successor)
 
 
+class QNodeGraph(QStandardItemModel):
+	def __init__(self):
+		super().__init__()
+
+
 if __name__ == "__main__":
-	item = QNodeGraphItem(text="Add", task=lambda a, b: a + b)
+	item = QNodeGraphItem(text="Add", task="lambda a, b: a + b")
 
 	print("Text:", item.text())
 	print("Has children:", item.hasChildren())
 	print("Task:", item.child(0, 0).data(Qt.DisplayRole))
 	print("Pre:", item.child(1, 0).data(Qt.DisplayRole))
 	print("Suc:", item.child(2, 0).data(Qt.DisplayRole))
+
+	model: QStandardItemModel = QNodeGraph()
+	root: QStandardItem = model.invisibleRootItem()
+	root.appendRow(item)
+
+	app = QApplication(sys.argv)
+	view = QTreeView()
+	view.setModel(model)
+	view.setAlternatingRowColors(True)
+	view.show()
+	sys.exit(app.exec_())
