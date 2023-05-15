8076728c0fc8c8b314ad001b6f5ba46945253d9f
j8sr0230 <ronny.scharf08@gmail.com>
2023-05-15 13:39:37 +0200

    Refactors project structure

--- src/codelink/app.py
+++ src/codelink/app.py
@@ -25,26 +25,16 @@ if __name__ == "__main__":
     node_editor_widget.resize(1200, 600)
     node_editor_widget.show()
 
-    node_1 = NodeItem()
-    node_1.setPos(QtCore.QPointF(31600, 31800))
-    node_editor_scene.add_node(node_1)
-
-    node_2 = NodeItem()
-    node_2.setPos(QtCore.QPointF(32200, 32050))
-    node_editor_scene.add_node(node_2)
-
-    node_3 = NodeItem()
-    node_3.setPos(QtCore.QPointF(31900, 32100))
-    node_editor_scene.add_node(node_3)
-
-    # with open("graph.json", 'w', encoding='utf8') as json_file:
-    #     json.dump(node_editor_scene.serialize_nodes(), json_file, indent=4)
+    # node_1 = NodeItem()
+    # node_1.setPos(QtCore.QPointF(31600, 31800))
+    # node_editor_scene.add_node(node_1)
     #
-    # # for node in node_editor_scene.nodes:
-    # #     node_editor_scene.remove_node(node)
+    # node_2 = NodeItem()
+    # node_2.setPos(QtCore.QPointF(32200, 32050))
+    # node_editor_scene.add_node(node_2)
     #
-    # with open("graph.json", 'r', encoding='utf8') as json_file:
-    #     nodes_dict: list[dict] = json.load(json_file)
-    #     node_editor_scene.deserialize_nodes(nodes_dict)
+    # node_3 = NodeItem()
+    # node_3.setPos(QtCore.QPointF(31900, 32100))
+    # node_editor_scene.add_node(node_3)
 
     sys.exit(app.exec_())
