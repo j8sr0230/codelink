fc89d25ac24cb699014048ba654245d2500ec34b
j8sr0230 <ronny.scharf08@gmail.com>
2023-05-23 18:20:27 +0200

    Refactors project structure

--- src/codelink/property_widget.py
+++ src/codelink/property_widget.py
@@ -102,7 +102,7 @@ class PropertyWidget(QtWidgets.QWidget):
 		)
 		self.focus_changed.emit(next_table_view)
 
-	@QtCore.Slot(QtWidgets.QTableViewr)
+	@QtCore.Slot(QtWidgets.QTableView)
 	def focus_down(self, current_table: QtWidgets.QTableView):
 		current_table.clearFocus()
 		current_table.clearSelection()
