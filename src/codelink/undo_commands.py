3d452eca70bcd509a68aadfe5d830c78ae2a7f9e
j8sr0230 <ronny.scharf08@gmail.com>
2023-07-05 12:38:15 +0200

    Fixes DeleteSelectedCommand by operating on indexes

--- src/codelink/undo_commands.py
+++ src/codelink/undo_commands.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING, Optional
+import importlib
 
 import PySide2.QtWidgets as QtWidgets
 
@@ -15,8 +16,11 @@ class DeleteSelectedCommand(QtWidgets.QUndoCommand):
 	def __init__(self, scene: DAGScene, parent: Optional[QtWidgets.QUndoCommand] = None):
 		super().__init__(parent)
 
+		dag_scene_cls: type = getattr(importlib.import_module("dag_scene"), "DAGScene")  # Hack: Prevents cyclic import
+
 		self._scene: DAGScene = scene
-		self._old_scene_dict: dict = dict()
+		self._undo_scene_dict: dict = self._scene.serialize()  # Serialize current scene
+		self._undo_scene: Optional[DAGScene] = dag_scene_cls()  # Empty new scene instance
 
 		self._selected_nodes_idx: list[int] = [
 			scene.nodes.index(item) for item in scene.selectedItems() if type(item) == NodeItem
@@ -30,16 +34,25 @@ class DeleteSelectedCommand(QtWidgets.QUndoCommand):
 
 	def undo(self) -> None:
 		self._scene.clear_scene()
-		self._scene.deserialize(self._old_scene_dict)
+
+		for node in self._undo_scene.nodes:
+			self._scene.add_node(node)
+
+		for edge in self._undo_scene.edges:
+			self._scene.add_edge(edge)
+
+		for frame in self._undo_scene.frames:
+			self._scene.add_frame(frame)
 
 	def redo(self) -> None:
-		self._old_scene_dict: dict = self._scene.serialize()
-
-		for idx in self._selected_frames_idx:
-			self._scene.remove_frame(self._scene.frames[idx])
+		for idx in self._selected_nodes_idx:
+			self._scene.remove_node(self._scene.nodes[idx])
 
 		for idx in self._selected_edges_idx:
 			self._scene.remove_edge(self._scene.edges[idx])
 
-		for idx in self._selected_nodes_idx:
-			self._scene.remove_node(self._scene.nodes[idx])
+		for idx in self._selected_frames_idx:
+			self._scene.remove_frame(self._scene.frames[idx])
+
+		self._scene.update()
+		self._undo_scene.deserialize(self._undo_scene_dict)  # Deserialize undo_scene here for performance reasons
