3c2a81cbb1aa5873299e8dfc955ff23126cf1677
j8sr0230 <ronny.scharf08@gmail.com>
2023-08-10 21:17:27 +0200

    Refactors utils.py

--- src/codelink/editor_widget.py
+++ src/codelink/editor_widget.py
@@ -9,7 +9,6 @@ import PySide2.QtCore as QtCore
 import PySide2.QtWidgets as QtWidgets
 import PySide2.QtGui as QtGui
 
-from utils import find_key_values, replace_key_values
 from undo_commands import (
     AddNodeCommand, RemoveNodeFromFrameCommand, AddGrpNodeCommand, ResolveGrpNodeCommand, MoveNodesCommand,
     RemoveNodeCommand, AddEdgeCommand, RerouteEdgeCommand, RemoveEdgeCommand,  AddFrameCommand, RemoveFrameCommand,
@@ -555,6 +554,43 @@ class EditorWidget(QtWidgets.QGraphicsView):
             self.scene().selection_to_clipboard()
 
     def paste(self) -> None:
+
+        # Yields all values corresponding to a key (i.e. "UUID") in a nested dictionary
+        def find_key_values(state: dict, key: str) -> str:
+            if isinstance(state, list):
+                for i in state:
+                    for x in find_key_values(i, key):
+                        yield x
+
+            elif isinstance(state, dict):
+                if key in state:
+                    yield state[key]
+                for j in state.values():
+                    for x in find_key_values(j, key):
+                        yield x
+
+        # Replaces all occurrences of old_value with new_value for a given key in a nested dictionary
+        def replace_key_values(state: dict, key: str, old_value: str, new_value: str) -> None:
+            if isinstance(state, list):
+                for i in state:
+                    replace_key_values(i, key, old_value, new_value)
+
+            elif isinstance(state, dict):
+                if key in state:
+                    if type(state[key]) is list and key == "Link":
+                        if state[key][0] == old_value:
+                            state[key] = (new_value, state[key][1])
+                    elif type(state[key]) is list and key == "Framed Nodes UUID's":
+                        for idx, value in enumerate(state[key]):
+                            if value == old_value:
+                                state[key][idx] = new_value
+                    else:
+                        if state[key] == old_value:
+                            state[key] = new_value
+
+                for j in state.values():
+                    replace_key_values(j, key, old_value, new_value)
+
         try:
             clipboard_state: dict = json.loads(QtWidgets.QApplication.clipboard().text())
             paste_pos: QtCore.QPointF = self.scene().views()[0].mapToScene(
