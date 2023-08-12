06b5cf97a0d2ea6e770d8f8b1388951ec9659ae4
j8sr0230 <ronny.scharf08@gmail.com>
2023-08-12 10:03:01 +0200

    Adds test.py

--- src/codelink/test.py
+++ src/codelink/test.py
@@ -1,15 +1,21 @@
 import awkward as ak
 
-a: ak.Array = ak.Array([[[[50.0]]], [[50.0]], [50.0]])
-b: ak.Array = ak.Array([0])
-c: ak.Array = ak.Array([0])
+from utils import simplify
+
+a: ak.Array = ak.Array(simplify([[[[50.0]]], [[50.0]], [50.0]]))
+b: ak.Array = ak.Array(simplify([0]))
+c: ak.Array = ak.Array(simplify([0]))
+
+print("a", a.to_list())
+print("b", b.to_list())
+print("c", c.to_list())
 
 a_cast: ak.Array = ak.broadcast_arrays(a, b, c)[0]
 b_cast: ak.Array = ak.broadcast_arrays(a, b, c)[1]
 c_cast: ak.Array = ak.broadcast_arrays(a, b, c)[2]
 
-print(a_cast.to_list())
-print(b_cast.to_list())
-print(c_cast.to_list())
+print("a_cast", a_cast.to_list())
+print("b_cast", b_cast.to_list())
+print("c_cast", c_cast.to_list())
 
-print(ak.zip([a_cast, b_cast, c_cast]))
+print("zip", ak.zip([a_cast, b_cast, c_cast]))
