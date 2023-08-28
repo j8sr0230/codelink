b06afb50df1cd55e48065f88d338af3cfd3c7570
j8sr0230 <ronny.scharf08@gmail.com>
2023-08-28 10:42:45 +0200

    Adds transform_shape.py

---
+++ src/codelink/nodes/shape/transform_shape.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+# ***************************************************************************
+# *   Copyright (c) 2023 Ronny Scharf-W. <ronny.scharf08@gmail.com>         *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+from __future__ import annotations
+from typing import TYPE_CHECKING, Optional
+import warnings
+
+import FreeCAD
+import Part
+
+import PySide2.QtWidgets as QtWidgets
+
+from utils import map_objects, broadcast_data_tree
+from node_item import NodeItem
+from vector_none import VectorNone
+from shape_none import ShapeNone
+
+if TYPE_CHECKING:
+    from socket_widget import SocketWidget
+
+
+class TransformShape(NodeItem):
+    REG_NAME: str = "Transform Shape"
+
+    def __init__(self, pos: tuple, undo_stack: QtWidgets.QUndoStack, name: str = REG_NAME,
+                 parent: Optional[QtWidgets.QGraphicsItem] = None) -> None:
+        super().__init__(pos, undo_stack, name, parent)
+
+        # Socket widgets
+        self._socket_widgets: list[SocketWidget] = [
+            ShapeNone(undo_stack=self._undo_stack, name="Shape", content_value="<No Input>", is_input=True,
+                      parent_node=self),
+            VectorNone(undo_stack=self._undo_stack, name="Translation", content_value="<No Input>", is_input=True,
+                       parent_node=self),
+            ShapeNone(undo_stack=self._undo_stack, name="Shape", content_value="<No Input>", is_input=False,
+                      parent_node=self)
+        ]
+
+    # --------------- Node eval methods ---------------
+
+    @staticmethod
+    def make_translation(parameter_zip: tuple) -> Part.Shape:
+        shape: Part.Shape = parameter_zip[0]
+        translation: FreeCAD.Vector = parameter_zip[1]
+
+        copy: Part.Shape = Part.Shape(shape)
+        return copy.translate(translation)
+
+    def eval_0(self, *args) -> list:
+        result: list = [Part.Shape()]
+
+        with warnings.catch_warnings():
+            warnings.filterwarnings("error")
+            try:
+                try:
+                    shape: list = self.input_data(0, args)
+                    translation: list = self.input_data(1, args)
+
+                    data_tree: list = list(broadcast_data_tree(shape, translation))
+                    result: list = list(map_objects(data_tree, tuple, self.make_translation))
+
+                    self._is_dirty: bool = False
+
+                except Exception as e:
+                    self._is_dirty: bool = True
+                    print(e)
+            except Warning as e:
+                self._is_dirty: bool = True
+                print(e)
+
+        return self.output_data(0, result)
