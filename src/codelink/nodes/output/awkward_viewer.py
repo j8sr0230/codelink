33bcc26d705593dcafe59348f73a352173ae57de
j8sr0230 <ronny.scharf08@gmail.com>
2023-09-04 08:35:38 +0200

    Simplifies awkward_viewer.py

--- src/codelink/nodes/output/awkward_viewer.py
+++ src/codelink/nodes/output/awkward_viewer.py
@@ -59,7 +59,7 @@ class AwkwardViewer(NodeItem):
             warnings.filterwarnings("error")
             try:
                 try:
-                    result: ak.Array = ak.Array(self.input_data(0, args))
+                    result: ak.Array = self.input_data(0, args)
                     result.show(limit_rows=100, limit_cols=100)
                     self._is_dirty: bool = False
 
