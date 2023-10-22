3ce5014f4bac280b8150f6869c7cccee265cb9b9
j8sr0230 <ronny.scharf08@gmail.com>
2023-10-22 18:08:46 +0200

    Tests numba

--- src/codelink/awkward_test.py
+++ src/codelink/awkward_test.py
@@ -24,6 +24,7 @@ import time
 
 import awkward as ak
 import numpy as np
+from numba import jit
 
 from utils import simplify
 
@@ -82,3 +83,16 @@ res.show()
 print()
 k: ak.Array = ak.Array([[99], [[[3, 4, 87]]], np.arange(0, 5)])
 print(simplify(k.to_list()))
+
+x = np.arange(100).reshape(10, 10)
+
+
+@jit(nopython=True)  # Set "nopython" mode for best performance, equivalent to @njit
+def go_fast(a):  # Function is compiled to machine code when called the first time
+    trace = 0.0
+    for i in range(a.shape[0]):  # Numba likes loops
+        trace += np.tanh(a[i, i])  # Numba likes NumPy functions
+    return a + trace  # Numba likes NumPy broadcasting
+
+
+print(go_fast(x))
