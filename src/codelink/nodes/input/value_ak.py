d1e8def939dbda55c369982b3e1dded1c8d476c8
j8sr0230 <ronny.scharf08@gmail.com>
2023-11-03 16:33:56 +0100

    Adds value_ak.py

---
+++ src/codelink/nodes/input/value_ak.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# ***************************************************************************
+# *   Copyright (c) 2023 Ronny Scharf-W. <ronny.scharf08@gmail.com>         *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+from __future__ import annotations
+from typing import TYPE_CHECKING, Optional
+import warnings
+import inspect
+
+import PySide2.QtWidgets as QtWidgets
+
+from node_item import NodeItem
+from sockets.value_line_ak import ValueLineAk
+
+if TYPE_CHECKING:
+    from socket_widget import SocketWidget
+
+
+class ValueAk(NodeItem):
+    REG_NAME: str = "ValueAk"
+
+    def __init__(self, pos: tuple, undo_stack: QtWidgets.QUndoStack, name=REG_NAME,
+                 parent: Optional[QtWidgets.QGraphicsItem] = None) -> None:
+        super().__init__(pos, undo_stack, name, parent)
+
+        # Socket widgets
+        self._socket_widgets: list[SocketWidget] = [
+            ValueLineAk(undo_stack=self._undo_stack, name="Value", content_value=0., is_input=True, parent_node=self),
+            ValueLineAk(undo_stack=self._undo_stack, name="Value", content_value="<No Input>", is_input=False,
+                        parent_node=self)
+        ]
+
+    # --------------- Node eval methods ---------------
+
+    def eval_0(self, *args) -> list:
+        cache_idx: int = int(inspect.stack()[0][3].split("_")[-1])
+
+        if self._is_invalid or self._cache[cache_idx] is None:
+
+            with warnings.catch_warnings():
+                warnings.filterwarnings("error")
+                try:
+                    try:
+                        value_in: list = self.input_data(0, args)
+                        result: list = value_in
+
+                        self._is_dirty: bool = False
+                        self._is_invalid: bool = False
+                        self._cache[cache_idx] = self.output_data(0, result)
+                        print("Value executed")
+
+                    except Exception as e:
+                        self._is_dirty: bool = True
+                        print(e)
+                except Warning as e:
+                    self._is_dirty: bool = True
+                    print(e)
+
+        return self._cache[cache_idx]
