5c2de9e808c3c1bba67cf238007f5a94139aef2d
j8sr0230 <ronny.scharf08@gmail.com>
2023-11-03 10:35:31 +0100

    Improves scalar_functions_ak.py

--- src/codelink/nested_data.py
+++ src/codelink/nested_data.py
@@ -34,10 +34,17 @@ class NestedData:
 def nd_zip(*args):
 	structure_dict: dict[Any] = {str(idx): item.structure for idx, item in enumerate(args)}
 	structure_zip: ak.Array = ak.zip(structure_dict)
-	print(structure_zip)
+	structure_zip.show()
 
-	# a_flat: ak.Array = ak.flatten(res_structure.a, axis=None)
-	# b_flat: ak.Array = ak.flatten(res_structure.b, axis=None)
+	flat_structure_lists: list[ak.Array] = [ak.flatten(structure_zip[key], axis=None) for key in structure_dict]
+	flat_structure_tuples: list[tuple[int, int]] = ak.to_list(ak.zip(flat_structure_lists))
+
+	for structure_tuple in flat_structure_tuples:
+		print(
+			[structure_zip[str(key)][index] for key, index in enumerate(structure_tuple)]
+		)
+
+	# (structure_zip[key][idx] for key, idx in enumerate(structure_tuple))
 
 
 def main() -> None:
@@ -68,10 +75,10 @@ def main() -> None:
 
 	params: list[tuple[Any, Any]] = [(a.data[ak.to_list(item)[0]], b.data[ak.to_list(item)[1]]) for item in res_flat]
 	print(params)
+	print()
 
 	nd_zip(a, b)
 
 
 if __name__ == "__main__":
 	main()
-
