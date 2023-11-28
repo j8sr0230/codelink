40ea6f97ed7dfefef1f6a768d5710f843767534f
j8sr0230 <ronny.scharf08@gmail.com>
2023-11-28 09:49:17 +0100

    Introduces vector_to_nested and nested_to_vector methods in utils.py

--- src/codelink/nested_data.py
+++ src/codelink/nested_data.py
@@ -1,16 +1,31 @@
-from typing import Union
+from typing import Union, cast
 
 import awkward as ak
-# import numpy as np
+import numpy as np
+
+
+# noinspection PyUnusedLocal
+def global_index(layout: ak.contents.Content, **kwargs) -> ak.contents.Content:
+	if layout.is_numpy:
+		layout: np.ndarray = cast(np.ndarray, layout)
+		# noinspection PyTypeChecker
+		return ak.contents.NumpyArray(
+			np.arange(0, layout.data.size)
+		)
 
 
 class NestedData:
-	def __init__(self, data: Union[ak.Array, list] = None, structure: ak.Array = ak.Array([0])):
-		if data is None:
-			data: Union[ak.Array, list] = []
-
-		self._data: Union[ak.Array, list] = data
-		self._structure: Union[ak.Array, float] = structure
+	def __init__(self, data: Union[ak.Array, list] = ak.Array([0]), structure: ak.Array = ak.Array([0])):
+		if type(data) is ak.Array and data.layout.minmax_depth[0] != 1:
+			self._data: ak.Array = ak.zip({
+				"x": ak.flatten(data.x, axis=None),
+				"y": ak.flatten(data.y, axis=None),
+				"z": ak.flatten(data.z, axis=None)
+			})
+			self._structure: ak.Array = ak.transform(global_index, data.x)
+		else:
+			self._data: Union[ak.Array, list] = data
+			self._structure: Union[ak.Array, float] = structure
 
 	@property
 	def data(self) -> Union[ak.Array, list]:
@@ -28,17 +43,30 @@ class NestedData:
 	def structure(self, value: ak.Array) -> None:
 		self._structure: ak.Array = value
 
-	def data_keep_last(self) -> ak.Array:
-		# TODO: Gets view with last level preserved
-		pass
+	def data_keep_last(self) -> Union[ak.Array, list]:
+		if type(self._data) is ak.Array:
+			# This works
+			original_data: ak.Array = ak.zip({"x": self._structure, "y": self._structure, "z": self._structure})
+
+			# This not
+			original_data["x"] = self._data.x
+			original_data["y"] = self._data.y
+			original_data["z"] = self._data.z
+
+			if original_data.layout.minmax_depth[0] > 2:
+				while original_data.layout.minmax_depth[0] > 2:
+					original_data: ak.Array = ak.flatten(original_data, axis=1)
+
+			return original_data
 
 	def structure_keep_last(self) -> ak.Array:
-		# TODO: Gets view with last level preserved
-		pass
+		struct: Union[float, ak.Array] = ak.sum(self._structure)
+		if type(struct) == ak.Array:
+			struct: ak.Array = ak.transform(global_index, struct)
+		return struct
 
 	def __str__(self) -> str:
 		return "Data: " + str(self._data) + " / Structure: " + str(self._structure)
-
 
 # def nd_zip(*args) -> tuple[list[list[Any, ...]], ak.Array]:
 # 	structure_dict: dict[str, Any] = {str(idx): item.structure for idx, item in enumerate(args)}
