a6c6ecebcb8359d2e215919450d40a3564cf5649
j8sr0230 <ronny.scharf08@gmail.com>
2023-11-30 14:23:57 +0100

    Improves wheel event to NumberInputWidget

--- src/codelink/input_widgets.py
+++ src/codelink/input_widgets.py
@@ -52,7 +52,7 @@ class NumberInputWidget(QtWidgets.QLineEdit):
             super().keyPressEvent(event)
             if event.key() == QtCore.Qt.Key_Return:
                 self.deselect()
-                self.parentWidget().parentWidget().setFocus()
+                QtWidgets.QApplication.topLevelWidgets()[0].setFocus()
 
     def wheelEvent(self, event: QtGui.QWheelEvent) -> None:
         event.accept()
--- src/codelink/sockets/value_line.py
+++ src/codelink/sockets/value_line.py
@@ -123,7 +123,6 @@ class ValueLine(SocketWidget):
 	def update_all(self):
 		super().update_all()
 		self._input_widget.setText(str(self._prop_model.properties["Value"]))
-		self.clearFocus()
 
 	def validate_input(self):
 		last_value: float = self.prop_model.properties["Value"]
