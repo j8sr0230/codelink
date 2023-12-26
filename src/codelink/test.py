922a6cd182445e27f6fd8fd8a66e14d7ad27a355
j8sr0230 <ronny.scharf08@gmail.com>
2023-12-26 14:02:37 +0100

    Adds and validates kuka kr6 kinematic

--- src/codelink/test.py
+++ src/codelink/test.py
@@ -1,17 +1,73 @@
-import awkward as ak
+import numpy as np
+from ikpy.chain import Chain
+from ikpy.link import OriginLink, URDFLink
+import matplotlib.pyplot
+from mpl_toolkits.mplot3d import Axes3D  # noqa
 
-from utils import flatten_array, unflatten_array_like
 
-print("a")
-a: ak.Array = ak.Array([[[1, 2, 9, 100]], [[3, 4]], [[5]]])
-a.show()
-print()
+kuka_kr_6_chain: Chain = Chain(name="kuka_kr_6", links=[
+	OriginLink(),
+	URDFLink(
+		name="A1",
+		origin_translation=np.array([0, 0, 0]),
+		origin_orientation=np.array([0, 0, 0]),
+		rotation=np.array([0, 0, 1]),
+	),
+	URDFLink(
+		name="A2",
+		origin_translation=np.array([260, 0, 675]),
+		origin_orientation=np.array([0, 0, 0]),
+		rotation=np.array([0, 1, 0]),
+	),
+	URDFLink(
+		name="A3",
+		origin_translation=np.array([0, 0, 680]),
+		origin_orientation=np.array([0, 0, 0]),
+		rotation=np.array([0, 1, 0]),
+	),
+	URDFLink(
+		name="A4",
+		origin_translation=np.array([670, 0, -35]),
+		origin_orientation=np.array([0, 0, 0]),
+		rotation=np.array([1, 0, 0]),
+	),
+	URDFLink(
+		name="A5",
+		origin_translation=np.array([0, 0, 0]),
+		origin_orientation=np.array([0, 0, 0]),
+		rotation=np.array([0, 1, 0]),
+	),
+	URDFLink(
+		name="A6",
+		origin_translation=np.array([115, 0, 0]),
+		origin_orientation=np.array([0, 0, 0]),
+		rotation=np.array([1, 0, 0]),
+	)
+], active_links_mask=[False, True, True, True, True, True, True])
 
-print("flat a")
-flat_a: ak.Array = flatten_array(a)
-flat_a.show()
-print()
+a1: np.ndarray = np.radians(0)
+a2: np.ndarray = np.radians(0)
+a3: np.ndarray = np.radians(20)
+a4: np.ndarray = np.radians(0)
+a5: np.ndarray = np.radians(90)
+a6: np.ndarray = np.radians(0)
 
-print("deep a")
-deep_a: ak.Array = unflatten_array_like(flat_array=flat_a, template_array=a)
-deep_a.show()
+# Forward kinematic
+pos_ori: np.ndarray = kuka_kr_6_chain.forward_kinematics(joints=[0, a1, a2, a3, a4, a5, a6])
+print("Position for", np.degrees([a1, a2, a3, a4, a5, a6]), "deg ->", np.round(pos_ori[:3, 3], 2), "mm")
+
+# Inverse kinematic
+axes: np.array = kuka_kr_6_chain.inverse_kinematics(
+	target_position=pos_ori[:3, 3],
+	# target_orientation=pos_ori[:3, :3],
+	# orientation_mode="all"
+	target_orientation=np.degrees([1, 0, 0]),
+	orientation_mode="Z"
+)
+print("Axes for", np.round(np.round(pos_ori[:3, 3], 2), 2), " mm ->", np.round(np.degrees(axes[1:]), 2), "deg")
+
+# Plotting forward and inverse result
+ax = matplotlib.pyplot.figure().add_subplot(111, projection='3d')
+kuka_kr_6_chain.plot([0, a1, a2, a3, a4, a5, a6], ax)
+kuka_kr_6_chain.plot(axes, ax)
+matplotlib.pyplot.show()
