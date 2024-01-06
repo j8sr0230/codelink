b3c2d73170f9cea50b563eba4955088ede8eddc4
j8sr0230 <ronny.scharf08@gmail.com>
2024-01-06 20:51:01 +0100

    Adds boolean.py

--- src/codelink/utils.py
+++ src/codelink/utils.py
@@ -201,6 +201,7 @@ def mass_zip_to_array(nested_array: ak.Array) -> ak.Array:
     zipped_tuples: ak.Array = ak.zip(ak.to_list(nested_array), right_broadcast=True)
     grafted_tuples: ak.Array = ak.unflatten(zipped_tuples, counts=1, axis=-1)
     return ak.concatenate(ak.unzip(grafted_tuples), axis=-1)
+    # return ak.Array(ak.to_list(ak.to_list(zipped_tuples)))
 
 
 def reorder_list(flat_list: list[Any], target_structure: ak.Array) -> list:
