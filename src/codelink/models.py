44b0790127c7f69e5c5c1a440fbbd077b268778a
j8sr0230 <ronny.scharf08@gmail.com>
2024-02-02 20:29:56 +0100

    Builds v0.0.26

--- src/codelink/models.py
+++ src/codelink/models.py
@@ -1,8 +1,6 @@
 # Copyright (c) 2024, Ronny Scharf-W. (ronny.scharf08@gmail.com)
 # LGPL-2.1 License, see https://github.com/j8sr0230/codelink/blob/main/LICENSE
 
-from __future__ import annotations
-from typing import Optional
 import uuid
 
 
@@ -12,61 +10,60 @@ GRAFT = 2
 
 
 class Edge:
-    def __init__(self, start_socket: Socket, end_socket: Socket, idx: Optional[uuid.UUID] = None) -> None:
-        self.start_socket: Socket = start_socket
-        self.end_socket: Socket = end_socket
-        self.idx: uuid.UUID = uuid.uuid1() if idx is None else idx
+    def __init__(self, start_socket, end_socket, idx=None):
+        self.start_socket = start_socket
+        self.end_socket = end_socket
+        self.idx = uuid.uuid1() if idx is None else idx
 
-    def __eq__(self, other: Edge) -> bool:
+    def __eq__(self, other):
         return self.idx == other.idx
 
-    def __str__(self) -> str:
-        return "Edge: <" + str(self.idx) + ", " + self.start_socket.name + ", " + self.end_socket.name + ">"
+    def __str__(self):
+        return "Edge: <" + str(self.idx) + ", " + str(self.start_socket) + ", " + str(self.end_socket) + ">"
 
 
 class Socket:
-    def __init__(self, name: str = "Socket", data_type: type = int, modifiers: Optional[list[int]] = None,
-                 edges: Optional[list[Edge]] = None, idx: Optional[uuid.UUID] = None) -> None:
+    def __init__(self, name="Socket", data_type=object, modifiers=None, edges=None, idx=None):
         self.name = name
         self.data_type = data_type
         self.modifiers = [] if modifiers is None else modifiers
         self.edges = [] if edges is None else edges
         self.idx = uuid.uuid1() if idx is None else idx
 
-    def __eq__(self, other: Socket) -> bool:
+    def __eq__(self, other):
         return self.idx == other.idx
 
-    def __str__(self) -> str:
-        return str("Socket: <" + str(self.idx) + ", " + str(self.name) + ", " + str(self.data_type) + ", " +
-                   str([m for m in self.modifiers]) + ", " + str([e.idx for e in self.edges]) + ">")
+    def __str__(self):
+        return ("Socket: <" + str(self.idx) + ", " + self.name + ", " + str(self.data_type) + ", " +
+                str(self.modifiers) + ", " + str(self.edges) + ">")
 
 
-def add_edge(start_socket: Socket, end_socket: Socket, idx: Optional[uuid.UUID] = None) -> Optional[Edge]:
-    all_edges: list[Edge] = start_socket.edges + end_socket.edges
-    all_socket: list[Socket] = [edge.start_socket for edge in all_edges] + [edge.end_socket for edge in all_edges]
-
-    if not (start_socket in all_socket and end_socket in all_socket):
+def add_edge(start_socket, end_socket, idx=None):
+    if start_socket == end_socket:
+        return None
+    elif start_socket.data_type != end_socket.data_type:
+        return None
+    elif (end_socket in [e.end_socket for e in start_socket.edges] or
+          end_socket in [e.start_socket for e in end_socket.edges]):
+        print("here")
+        return None
+    else:
         edge = Edge(start_socket, end_socket, idx)
         start_socket.edges.append(edge)
         end_socket.edges.append(edge)
         return edge
 
 
-def remove_edge(edge) -> Edge:
-    start_socket: Socket = edge.start_socket
-    if edge in start_socket.edges:
-        start_socket.edges.remove(edge)
-
-    end_socket: Socket = edge.end_socket
-    if edge in end_socket.edges:
-        end_socket.edges.remove(edge)
-
+def remove_edge(edge):
+    edge.start_socket.edges.remove(edge)
+    edge.end_socket.edges.remove(edge)
     return edge
 
 
-s1 = Socket(name="s1", data_type=float, modifiers=[SIMPLIFY, GRAFT], edges=None)
+s1 = Socket("s1")
 s2 = Socket("s2")
 s3 = Socket("s3")
+
 print(s1)
 print(s2)
 print(s3)
@@ -79,19 +76,8 @@ print(s2)
 print(s3)
 print()
 
-add_edge(s2, s1)
-print(s1)
-print(s2)
-print(s3)
-print()
-
-add_edge(s2, s3)
-print(s1)
-print(s2)
-print(s3)
-print()
-
-remove_edge(e1)
+e1: Edge = add_edge(s2, s1)
+print(e1)
 print(s1)
 print(s2)
 print(s3)
