9b2bd14ea155c5a16301045157885eeacedb7e94
j8sr0230 <ronny.scharf08@gmail.com>
2024-02-02 08:42:27 +0100

    Improves edge model

--- src/codelink/egde.py
+++ src/codelink/egde.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2024 Ronny Scharf-W. <ronny.scharf08@gmail.com>
-# LGPL-2.1 License; see https://github.com/j8sr0230/codelink/blob/main/LICENSE
+# Copyright (c) 2024, Ronny Scharf-W. (ronny.scharf08@gmail.com)
+# LGPL-2.1 License, see https://github.com/j8sr0230/codelink/blob/main/LICENSE
 
 class Edge:
     def __init__(self):
