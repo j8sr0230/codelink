9647229b9a7c83c25f131a50a55e45ba595cf8f6
j8sr0230 <ronny.scharf08@gmail.com>
2024-02-05 05:39:05 +0100

    Adds models.py

--- src/codelink/models.py
+++ src/codelink/models.py
@@ -1,84 +1,169 @@
-# Copyright (c) 2024, Ronny Scharf-W. (ronny.scharf08@gmail.com)
-# LGPL-2.1 License, see https://github.com/j8sr0230/codelink/blob/main/LICENSE
+from __future__ import annotations
+from typing import Any, Optional, Union, cast
 
-import uuid
-
-
-FLATTEN = 0
-SIMPLIFY = 1
-GRAFT = 2
+from PySide2 import QtCore, QtWidgets
 
 
-class Edge:
-    def __init__(self, start_socket, end_socket, idx=None):
-        self.start_socket = start_socket
-        self.end_socket = end_socket
-        self.idx = uuid.uuid1() if idx is None else idx
+class TreeItem:
+    def __init__(self, data: list[str],  parent: TreeItem = None) -> None:
+        self.parentItem = parent
+        self.itemData = data
+        self.childItems = []
 
-    def __eq__(self, other):
-        return self.idx == other.idx
+    def append_child(self, item: TreeItem) -> None:
+        self.childItems.append(item)
 
-    def __str__(self):
-        return "Edge: <" + str(self.idx) + ", " + str(self.start_socket) + ", " + str(self.end_socket) + ">"
+    def child(self, row) -> TreeItem:
+        return self.childItems[row]
+
+    def child_count(self) -> int:
+        return len(self.childItems)
+
+    def column_count(self) -> int:
+        return len(self.itemData)
+
+    def data(self, column: int) -> Optional[str]:
+        try:
+            return self.itemData[column]
+        except IndexError:
+            return None
+
+    def parent(self) -> Any:
+        return self.parentItem
+
+    def row(self) -> int:
+        if self.parentItem:
+            return self.parentItem.childItems.index(self)
+
+        return 0
 
 
-class Socket:
-    def __init__(self, name="Socket", data_type=object, modifiers=None, edges=None, idx=None):
-        self.name = name
-        self.data_type = data_type
-        self.modifiers = [] if modifiers is None else modifiers
-        self.edges = [] if edges is None else edges
-        self.idx = uuid.uuid1() if idx is None else idx
+class TreeModel(QtCore.QAbstractItemModel):
+    def __init__(self, data: str, parent: Optional[QtCore.QObject] = None):
+        super(TreeModel, self).__init__(parent)
 
-    def __eq__(self, other):
-        return self.idx == other.idx
+        self.rootItem = TreeItem(["Title", "Summary"])
+        self.setup_model_data(data.split('\n'), self.rootItem)
 
-    def __str__(self):
-        return ("Socket: <" + str(self.idx) + ", " + self.name + ", " + str(self.data_type) + ", " +
-                str(self.modifiers) + ", " + str(self.edges) + ">")
+    def columnCount(self, parent: QtCore.QModelIndex = QtCore.QModelIndex()) -> int:
+        if parent.isValid():
+            return cast(parent.internalPointer(), TreeItem).column_count()
+        else:
+            return self.rootItem.column_count()
 
+    def data(self, index: QtCore.QModelIndex, role: int = QtCore.Qt.DisplayRole) -> Optional[str]:
+        if not index.isValid():
+            return None
 
-def add_edge(start_socket, end_socket, idx=None):
-    if start_socket == end_socket:
+        if role != QtCore.Qt.DisplayRole:
+            return None
+
+        item: TreeItem = cast(index.internalPointer(), TreeItem)
+
+        return item.data(index.column())
+
+    def flags(self, index: QtCore.QModelIndex) -> Union[QtCore.Qt.ItemFlag, QtCore.Qt.ItemFlags]:
+        if not index.isValid():
+            return QtCore.Qt.NoItemFlags
+
+        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+
+    def headerData(self, section: int, orientation: QtCore.Qt.Orientation, role: int = QtCore.Qt.DisplayRole) -> Any:
+        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
+            return self.rootItem.data(section)
+
         return None
-    elif start_socket.data_type != end_socket.data_type:
-        return None
-    elif (end_socket in [e.end_socket for e in start_socket.edges] or
-          end_socket in [e.start_socket for e in end_socket.edges]):
-        print("here")
-        return None
-    else:
-        edge = Edge(start_socket, end_socket, idx)
-        start_socket.edges.append(edge)
-        end_socket.edges.append(edge)
-        return edge
+
+    def index(self, row: int, column: int, parent: QtCore.QModelIndex = QtCore.QModelIndex()) -> QtCore.QModelIndex:
+        if not self.hasIndex(row, column, parent):
+            return QtCore.QModelIndex()
+
+        if not parent.isValid():
+            parent_item: TreeItem = self.rootItem
+        else:
+            parent_item: TreeItem = cast(parent.internalPointer(), TreeItem)
+
+        child_item: TreeItem = parent_item.child(row)
+        if child_item:
+            return self.createIndex(row, column, child_item)
+        else:
+            return QtCore.QModelIndex()
+
+    def parent(self, index: QtCore.QModelIndex = QtCore.QModelIndex()) -> QtCore.QModelIndex:
+        if not index.isValid():
+            return QtCore.QModelIndex()
+
+        child_item: TreeItem = cast(index.internalPointer(), TreeItem)
+        parent_item: TreeItem = child_item.parent()
+
+        if parent_item == self.rootItem:
+            return QtCore.QModelIndex()
+
+        return self.createIndex(parent_item.row(), 0, parent_item)
+
+    def rowCount(self, parent: QtCore.QModelIndex = QtCore.QModelIndex()) -> int:
+        if parent.column() > 0:
+            return 0
+
+        if not parent.isValid():
+            parent_item: TreeItem = self.rootItem
+        else:
+            parent_item: TreeItem = cast(parent.internalPointer(), TreeItem)
+
+        return parent_item.child_count()
+
+    @staticmethod
+    def setup_model_data(lines: list[str], parent: TreeItem) -> None:
+        parents = [parent]
+        indentations = [0]
+
+        number = 0
+
+        while number < len(lines):
+            position = 0
+            while position < len(lines[number]):
+                if lines[number][position] != ' ':
+                    break
+                position += 1
+
+            line_data = lines[number][position:].strip()
+
+            if line_data:
+                # Read the column data from the rest of the line.
+                column_data = [s for s in line_data.split('\t') if s]
+
+                if position > indentations[-1]:
+                    # The last child of the current parent is now the new
+                    # parent unless the current parent has no children.
+
+                    if parents[-1].child_count() > 0:
+                        parents.append(parents[-1].child(parents[-1].child_count() - 1))
+                        indentations.append(position)
+
+                else:
+                    while position < indentations[-1] and len(parents) > 0:
+                        parents.pop()
+                        indentations.pop()
+
+                # Append a new item to the current parent's list of children.
+                parents[-1].append_child(TreeItem(column_data, parents[-1]))
+
+            number += 1
 
 
-def remove_edge(edge):
-    edge.start_socket.edges.remove(edge)
-    edge.end_socket.edges.remove(edge)
-    return edge
+if __name__ == '__main__':
 
+    import sys
 
-s1 = Socket("s1")
-s2 = Socket("s2")
-s3 = Socket("s3")
+    app: QtWidgets.QApplication = QtWidgets.QApplication(sys.argv)
 
-print(s1)
-print(s2)
-print(s3)
-print()
+    f: QtCore.QFile = QtCore.QFile(':/default.txt')
+    f.open(QtCore.QIODevice.ReadOnly)
+    model: TreeModel = TreeModel(str(f.readAll()))
+    f.close()
 
-e1: Edge = add_edge(s1, s2)
-print(e1)
-print(s1)
-print(s2)
-print(s3)
-print()
-
-e1: Edge = add_edge(s2, s1)
-print(e1)
-print(s1)
-print(s2)
-print(s3)
-print()
+    view: QtWidgets.QTreeView = QtWidgets.QTreeView()
+    view.setModel(model)
+    view.setWindowTitle("Simple Tree Model")
+    view.show()
+    sys.exit(app.exec_())
