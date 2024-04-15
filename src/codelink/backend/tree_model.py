d34abb1db0ea91812d86330466623abf5057a225
j8sr0230 <ronny.scharf08@gmail.com>
2024-04-15 13:46:29 +0200

    Improves tree_model.py

--- src/codelink/backend/tree_model.py
+++ src/codelink/backend/tree_model.py
@@ -26,8 +26,11 @@
 
 from __future__ import annotations
 from typing import cast, Any, Optional, Union
+import sys
 
 import PySide2.QtCore as QtCore
+import PySide2.QtGui as QtGui
+import PySide2.QtWidgets as QtWidgets
 
 from tree_item import TreeItem
 
@@ -36,15 +39,11 @@ class TreeModel(QtCore.QAbstractItemModel):
     def __init__(self, header: list[str], parent: QtCore.QObject = None):
         super().__init__(parent)
 
-        self.rootItem: TreeItem = TreeItem(header)
+        self._root_item: TreeItem = TreeItem(header)
 
-    def get_item(self, index: QtCore.QModelIndex) -> TreeItem:
-        if index.isValid():
-            item: Optional[TreeItem] = cast(TreeItem, index.internalPointer())
-            if item is not None:
-                return item
-
-        return self.rootItem
+    @property
+    def root_item(self) -> TreeItem:
+        return self._root_item
 
     def index(self, row: int, column: int, parent=QtCore.QModelIndex()) -> QtCore.QModelIndex:
         if parent.isValid() and parent.column() != 0:
@@ -56,6 +55,14 @@ class TreeModel(QtCore.QAbstractItemModel):
             return self.createIndex(row, column, child_item)
         else:
             return QtCore.QModelIndex()
+
+    def get_item(self, index: QtCore.QModelIndex) -> TreeItem:
+        if index.isValid():
+            item: Optional[TreeItem] = cast(TreeItem, index.internalPointer())
+            if item is not None:
+                return item
+
+        return self._root_item
 
     def data(self, index: QtCore.QModelIndex, role: int = QtCore.Qt.DisplayRole) -> Any:
         if not index.isValid():
@@ -80,17 +87,17 @@ class TreeModel(QtCore.QAbstractItemModel):
         return result
 
     def columnCount(self, parent: QtCore.QModelIndex = QtCore.QModelIndex()) -> int:
-        return self.rootItem.data_column_count()
+        return self._root_item.data_column_count()
 
     def insertColumns(self, position: int, columns: int, parent=QtCore.QModelIndex()) -> bool:
         self.beginInsertColumns(parent, position, position + columns - 1)
-        result: bool = self.rootItem.insert_data_columns(position, columns)
+        result: bool = self._root_item.insert_data_columns(position, columns)
         self.endInsertColumns()
         return result
 
     def removeColumns(self, position: int, columns: int, parent=QtCore.QModelIndex()) -> bool:
         self.beginRemoveColumns(parent, position, position + columns - 1)
-        result: bool = self.rootItem.remove_data_columns(position, columns)
+        result: bool = self._root_item.remove_data_columns(position, columns)
         self.endRemoveColumns()
 
         if self.rootItem.data_column_count() == 0:
@@ -105,7 +112,7 @@ class TreeModel(QtCore.QAbstractItemModel):
     def insertRows(self, position: int, rows: int, parent=QtCore.QModelIndex()) -> bool:
         parent_item: TreeItem = self.get_item(parent)
         self.beginInsertRows(parent, position, position + rows - 1)
-        result: bool = parent_item.insert_child_rows(position, rows, self.rootItem.data_column_count())
+        result: bool = parent_item.insert_child_rows(position, rows, self._root_item.data_column_count())
         self.endInsertRows()
         return result
 
@@ -123,7 +130,7 @@ class TreeModel(QtCore.QAbstractItemModel):
         child_item: TreeItem = self.get_item(index)
         parent_item: Optional[TreeItem] = child_item.parent
 
-        if parent_item == self.rootItem:
+        if parent_item == self._root_item:
             return QtCore.QModelIndex()
 
         return self.createIndex(parent_item.child_row_number(), 0, parent_item)
@@ -136,7 +143,7 @@ class TreeModel(QtCore.QAbstractItemModel):
 
     def headerData(self, section: int, orientation: QtCore.Qt.Orientation, role=QtCore.Qt.DisplayRole) -> Any:
         if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
-            return self.rootItem.data(section)
+            return self._root_item.data(section)
 
         return None
 
@@ -153,14 +160,36 @@ class TreeModel(QtCore.QAbstractItemModel):
 
 
 if __name__ == "__main__":
-    import PySide2.QtGui as QtGui
+    model: TreeModel = TreeModel(header=["Key", "Value"])
 
-    root_item: TreeItem = TreeItem(data=["Key", "Value"], parent=None)
-    root_item.insert_child_rows(position=0, count=1, columns=2)
+    model.dataChanged.connect(
+        lambda top_left_idx, bottom_right_idx, roles: print(model.data(top_left_idx))
+    )
 
-    data_item: TreeItem = root_item.child(0)
-    data_item.set_data(column=0, value="Color")
-    data_item.set_data(column=1, value=QtGui.QColor("#1D1D1D"))
+    model.insertRows(0, 1, QtCore.QModelIndex())
+    new_node_key_index: QtCore.QModelIndex = model.index(0, 0, model.createIndex(0, 0, model.root_item))
+    new_node_value_index: QtCore.QModelIndex = model.index(0, 1, model.createIndex(0, 0, model.root_item))
+    model.setData(new_node_key_index, "Name")
+    model.setData(new_node_value_index, "Node")
 
-    print(root_item.data(0), root_item.data(1))
-    print(data_item.child_row_number(), data_item.data(0), data_item.data(1))
+    model.insertRows(0, 1, new_node_key_index)
+    new_prop_key_index: QtCore.QModelIndex = model.index(0, 0, new_node_key_index)
+    new_prop_value_index: QtCore.QModelIndex = model.index(0, 1, new_node_key_index)
+    model.setData(new_prop_key_index, "Color")
+    model.setData(new_prop_value_index, QtGui.QColor("red"))
+
+    model.insertRows(1, 1, new_node_key_index)
+    new_prop_key_index: QtCore.QModelIndex = model.index(1, 0, new_node_key_index)
+    new_prop_value_index: QtCore.QModelIndex = model.index(1, 1, new_node_key_index)
+    model.setData(new_prop_key_index, "Input")
+    model.setData(new_prop_value_index, 5)
+
+    app = QtWidgets.QApplication(sys.argv)
+    main_window: QtWidgets.QMainWindow = QtWidgets.QMainWindow()
+
+    tree_view: QtWidgets.QTreeView = QtWidgets.QTreeView()
+    tree_view.setModel(model)
+    main_window.setCentralWidget(tree_view)
+    main_window.show()
+
+    sys.exit(app.exec_())
