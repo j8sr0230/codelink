f2145399c46d83adb4064aca8b54ef33328cae02
j8sr0230 <ronny.scharf08@gmail.com>
2024-06-05 14:36:26 +0200

    Improves main_window.py

--- src/codelink/frontend/main_window.py
+++ src/codelink/frontend/main_window.py
@@ -49,7 +49,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self._node_factory: NodeFactory = NodeFactory()
         self._node_factory.load_nodes(str(Path("../backend/nodes").resolve()))
-        self._model: TreeModel = self.create_tree_model(file=None)
+        self._tree_model: TreeModel = self.create_tree_model(file=None)
 
         self._file_name: Optional[str] = None
 
@@ -60,24 +60,27 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self._graphics_view: QtWidgets.QGraphicsView = self.create_graphics_view()
         self._main_tree_view: QtWidgets.QTreeView = self.create_main_tree_view()
-        self._inspection_view: QtWidgets.QTreeView = self.create_inspection_view()
+        self._inspection_tree_view: QtWidgets.QTreeView = self.create_inspection_tree_view()
 
     @property
-    def model(self) -> TreeModel:
-        return self._model
+    def tree_model(self) -> TreeModel:
+        return self._tree_model
 
     def create_tree_model(self, file: Optional[str] = None) -> QtCore.QAbstractItemModel:
         state: Optional[dict[str, Any]] = None
 
         if file:
-            with open(str(Path(file).resolve()), "r", encoding="utf-8") as f:
-                state: dict[str, Any] = json.load(f)
+            try:
+                with open(str(Path(file).resolve()), "r", encoding="utf-8") as f:
+                    state: dict[str, Any] = json.load(f)
+            except (FileNotFoundError, json.decoder.JSONDecodeError):
+                print("File loading error")
 
-        model: TreeModel = TreeModel(data=state, undo_stack=self._undo_stack)
-        model.rowsInserted.connect(self.on_model_row_changed)
-        model.rowsRemoved.connect(self.on_model_row_changed)
-        model.dataChanged.connect(self.on_model_data_changed)
-        return model
+        tree_model: TreeModel = TreeModel(data=state, undo_stack=self._undo_stack)
+        tree_model.rowsInserted.connect(self.on_model_row_changed)
+        tree_model.rowsRemoved.connect(self.on_model_row_changed)
+        tree_model.dataChanged.connect(self.on_model_data_changed)
+        return tree_model
 
     def create_menu(self) -> None:
         file_menu: QtWidgets.QMenu = self.menuBar().addMenu("&File")
@@ -134,7 +137,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def populate_nodes_menu(self, nodes_menu: QtWidgets.QMenu) -> None:
         def add_node(node_cls: str) -> None:
             node: NodeItem = self._node_factory.create_node(node_cls)
-            self._model.append_node(node)
+            self._tree_model.append_node(node)
 
         for key in self._node_factory.nodes.keys():
             parent_menu: QtWidgets.QMenu = nodes_menu
@@ -167,27 +170,29 @@ class MainWindow(QtWidgets.QMainWindow):
         dock: QtWidgets.QDockWidget = QtWidgets.QDockWidget("Graph View", self)
         dock.setAllowedAreas(QtCore.Qt.LeftDockWidgetArea | QtCore.Qt.RightDockWidgetArea)
         main_tree_view: TreeView = TreeView()
-        main_tree_view.setModel(self._model)
+        main_tree_view.setModel(self._tree_model)
         main_tree_view.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
-        self._model.rowsInserted.connect(
-            lambda: main_tree_view.expandRecursively(QtCore.QModelIndex())
-        )
+        # self._tree_model.rowsInserted.connect(
+        #     lambda: main_tree_view.expandRecursively(QtCore.QModelIndex())
+        # )
+        main_tree_view.expandAll()
         main_tree_view.selectionModel().selectionChanged.connect(self.on_selection_changed)
         dock.setWidget(main_tree_view)
         self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, dock)
         return main_tree_view
 
-    def create_inspection_view(self) -> QtWidgets.QTreeView:
+    def create_inspection_tree_view(self) -> QtWidgets.QTreeView:
         dock = QtWidgets.QDockWidget("Inspection View", self)
-        inspection_view: TreeView = TreeView()
-        inspection_view.setModel(self._model)
-        inspection_view.setIndentation(0)
-        self._model.rowsInserted.connect(
-            lambda: inspection_view.expandRecursively(QtCore.QModelIndex())
-        )
-        dock.setWidget(inspection_view)
+        inspection_tree_view: TreeView = TreeView()
+        inspection_tree_view.setModel(self._tree_model)
+        inspection_tree_view.setIndentation(0)
+        # self._tree_model.rowsInserted.connect(
+        #     lambda: inspection_tree_view.expandRecursively(QtCore.QModelIndex())
+        # )
+        inspection_tree_view.expandAll()
+        dock.setWidget(inspection_tree_view)
         self.addDockWidget(QtCore.Qt.RightDockWidgetArea, dock)
-        return inspection_view
+        return inspection_tree_view
 
     # noinspection PyUnusedLocal
     def on_model_data_changed(self, top_left: QtCore.QModelIndex, bottom_right: QtCore.QModelIndex,
@@ -212,41 +217,48 @@ class MainWindow(QtWidgets.QMainWindow):
     def on_selection_changed(self, current: QtCore.QItemSelection, previous: QtCore.QItemSelection) -> None:
         if len(current.indexes()) > 0:
             index: QtCore.QModelIndex = cast(QtCore.QModelIndex, current.indexes()[0])
-            tree_item: TreeItem = self._model.item_from_index(index)
+            tree_item: TreeItem = self._tree_model.item_from_index(index)
             if isinstance(tree_item, NodeItem):
-                self._inspection_view.setRootIndex(index)
+                self._inspection_tree_view.setRootIndex(index)
         else:
-            self._inspection_view.setRootIndex(QtCore.QModelIndex())
+            self._inspection_tree_view.setRootIndex(QtCore.QModelIndex())
 
     def _new(self, file: Optional[str]) -> None:
-        self._model: TreeModel = self.create_tree_model(file=file)
+        self._tree_model: TreeModel = self.create_tree_model(file=file)
         self._file_name: Optional[str] = file
         window_title: str = file if file else "CodeLink"
         self.setWindowTitle(window_title)
-        self._main_tree_view.setModel(self._model)
+
+        self._main_tree_view.setModel(self._tree_model)
         self._main_tree_view.selectionModel().selectionChanged.connect(self.on_selection_changed)
-        self._main_tree_view.expandAll()
-        self._inspection_view.setModel(self._model)
-        self._inspection_view.expandAll()
+        self._main_tree_view.expand(self._tree_model.nodes_index)
+        self._main_tree_view.expand(self._tree_model.edges_index)
+        self._main_tree_view.expand(self._tree_model.frames_index)
+
+        self._inspection_tree_view.setModel(self._tree_model)
+        self._inspection_tree_view.expand(self._tree_model.nodes_index)
+        self._inspection_tree_view.expand(self._tree_model.edges_index)
+        self._inspection_tree_view.expand(self._tree_model.frames_index)
         self._undo_stack.clear()
 
     def new(self) -> None:
         self._new(file=None)
 
     def open(self) -> None:
-        file_name: str = QtWidgets.QFileDialog.getOpenFileName(
+        file_name: tuple[str, str] = QtWidgets.QFileDialog.getOpenFileName(
             self, "Open file", "./", "Json files (*.json);;All files (*.*)"
-        )[0]
+        )
 
-        try:
-            self._new(file=file_name)
-        except (FileNotFoundError, json.decoder.JSONDecodeError, IndexError, ValueError, AttributeError):
-            print("File loading error")
+        if file_name[0]:
+            self._new(file=file_name[0])
+        else:
+            print("No file selected")
+
 
     def _save(self, file: str) -> None:
         try:
             with open(file, "w", encoding="utf-8") as f:
-                json.dump(self._model.to_dict(), f, ensure_ascii=False, indent=4)
+                json.dump(self._tree_model.to_dict(), f, ensure_ascii=False, indent=4)
 
             self._file_name: str = file
             self.setWindowTitle(file)
@@ -255,11 +267,14 @@ class MainWindow(QtWidgets.QMainWindow):
             print("File saving error")
 
     def save_as(self) -> None:
-        file_name: str = QtWidgets.QFileDialog.getSaveFileName(
+        file_name: tuple[str, str] = QtWidgets.QFileDialog.getSaveFileName(
             self, "Save file", "./", "Json files (*.json);;All files (*.*)"
-        )[0]
+        )
 
-        self._save(file_name)
+        if file_name[0]:
+            self._save(file_name[0])
+        else:
+            print("No file selected")
 
     def save(self) -> None:
         if self._file_name:
@@ -272,19 +287,19 @@ class MainWindow(QtWidgets.QMainWindow):
         while selected_indexes:
             selected_index: QtCore.QModelIndex = selected_indexes.pop()
             if selected_index.column() == 0:
-                tree_item: Optional[TreeItem] = self._model.item_from_index(selected_index)
+                tree_item: Optional[TreeItem] = self._tree_model.item_from_index(selected_index)
                 if isinstance(tree_item, NodeItem):
                     index: QtCore.QModelIndex = cast(QtCore.QModelIndex, selected_index)
-                    self._model.removeRow(index.row(), index.parent())
+                    self._tree_model.removeRow(index.row(), index.parent())
 
 
 if __name__ == "__main__":
     app: QtWidgets.QApplication = QtWidgets.QApplication(sys.argv)
     main_window: MainWindow = MainWindow()
 
-    main_window.model.append_node(NodeItem("Node 1"))
-    main_window.model.append_node(NodeItem("Node 2"))
-    main_window.model.append_node(NodeItem("Node 3"))
+    main_window.tree_model.append_node(NodeItem("Node 1"))
+    main_window.tree_model.append_node(NodeItem("Node 2"))
+    main_window.tree_model.append_node(NodeItem("Node 3"))
 
     main_window.show()
     sys.exit(app.exec_())
