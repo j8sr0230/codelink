a189ab1d92319a0107a0bc8d8d7c4f907156d957
j8sr0230 <ronny.scharf08@gmail.com>
2024-06-06 09:26:57 +0200

    Adds depth_filter_proxy_tree_model.py

---
+++ src/codelink/backend/depth_filter_proxy_tree_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+# ***************************************************************************
+# *   Copyright (c) 2024 Ronny Scharf-W. <ronny.scharf08@gmail.com>         *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+import PySide2.QtCore as QtCore
+
+
+class DepthFilterProxyTreeModel(QtCore.QSortFilterProxyModel):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def mapFromSource(self, source_index: QtCore.QModelIndex) -> QtCore.QModelIndex:
+        return super().mapFromSource(source_index)
+
+    def mapToSource(self, proxy_index: QtCore.QModelIndex) -> QtCore.QModelIndex:
+        return super().mapToSource(proxy_index)
+
+    def filterAcceptsRow(self, source_row: int, source_parent: QtCore.QModelIndex) -> bool:
+        return not source_parent.parent().isValid()
