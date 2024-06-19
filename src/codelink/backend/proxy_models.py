f2a8b6db7ac086917f46924e936edb2a962af217
j8sr0230 <ronny.scharf08@gmail.com>
2024-06-19 17:03:40 +0200

    Improves node_gr_item.py

--- src/codelink/backend/proxy_models.py
+++ src/codelink/backend/proxy_models.py
@@ -58,13 +58,14 @@ class ColumnSwapProxyModel(Level4ProxyModel):
         super().__init__()
 
     def mapFromSource(self, source_index: QtCore.QModelIndex) -> QtCore.QModelIndex:
-        if source_index.parent().row() == 2:
+        if source_index.parent().row() == 2 and source_index.parent().parent().parent().isValid():
             return super().mapFromSource(source_index).siblingAtColumn(abs(source_index.column() - 1))
         else:
             return super().mapFromSource(source_index)
 
+
     def mapToSource(self, proxy_index: QtCore.QModelIndex) -> QtCore.QModelIndex:
-        if proxy_index.parent().row() == 2:
+        if proxy_index.parent().row() == 2 and proxy_index.parent().parent().parent().isValid():
             return super().mapToSource(proxy_index).siblingAtColumn(abs(proxy_index.column() - 1))
         else:
             return super().mapToSource(proxy_index)
